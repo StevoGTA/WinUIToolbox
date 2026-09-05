f865e6a00cef7502ebd49250b1d2dc6b68354444
Stevo Brock <stevo@monkey-tools.com>
2026-09-04 22:45:51 -0700

    Reworked TextBoxHelper to be more correct.

--- Helpers/TextBoxHelper.cpp
+++ Helpers/TextBoxHelper.cpp
@@ -4,24 +4,16 @@
 
 #include "TextBoxHelper.h"
 
-#include "winrt\Microsoft.UI.Dispatching.h"
 #include "winrt\Microsoft.UI.Xaml.Controls.Primitives.h"
 #include "winrt\Microsoft.UI.Xaml.Input.h"
 #include "winrt\Microsoft.UI.Xaml.Media.h"
-#include "winrt\Windows.ApplicationModel.DataTransfer.h"
 #include "winrt\Windows.Foundation.h"
 #include "winrt\Windows.UI.h"
 
-using AsyncStatus = winrt::Windows::Foundation::AsyncStatus;
-using Clipboard = winrt::Windows::ApplicationModel::DataTransfer::Clipboard;
 using IInspectable = winrt::Windows::Foundation::IInspectable;
 using SolidColorBrush = winrt::Microsoft::UI::Xaml::Media::SolidColorBrush;
-using StandardDataFormats = winrt::Windows::ApplicationModel::DataTransfer::StandardDataFormats;
 using TextBoxTextChangingEventArgs = winrt::Microsoft::UI::Xaml::Controls::TextBoxTextChangingEventArgs;
 using TextChangedEventArgs = winrt::Microsoft::UI::Xaml::Controls::TextChangedEventArgs;
-using TextControlPasteEventArgs = winrt::Microsoft::UI::Xaml::Controls::TextControlPasteEventArgs;
-
-using IAsyncOperation_hstring = winrt::Windows::Foundation::IAsyncOperation<winrt::hstring>;
 
 //----------------------------------------------------------------------------------------------------------------------
 // MARK: Local data
@@ -91,58 +83,13 @@ TextBoxHelper& TextBoxHelper::setText(const winrt::param::hstring& string)
 }
 
 //----------------------------------------------------------------------------------------------------------------------
-TextBoxHelper& TextBoxHelper::setTextValidationProcs(std::function<bool(const hstring& string)> isAllowedProc,
-		std::function<hstring(const hstring& string)> filterProc)
+TextBoxHelper& TextBoxHelper::setTextFilterProc(std::function<hstring(const hstring& string)> filterProc,
+		std::function<void()> wasFilteredProc)
 //----------------------------------------------------------------------------------------------------------------------
 {
 	// Setup
-	getTextBox().Paste(
-			[isAllowedProc](const IInspectable& sender, const TextControlPasteEventArgs& textControlPasteEventArgs){
-				// Setup
-				auto	dataPackageView = Clipboard::GetContent();
-				if (!dataPackageView.Contains(StandardDataFormats::Text()))
-					return;
-
-				// Setup
-				auto	textBox = sender.as<TextBox>();
-
-				// Get text from clipboard
-				dataPackageView.GetTextAsync()
-						.Completed([textBox, isAllowedProc](const IAsyncOperation_hstring& asyncOperation,
-								const AsyncStatus& asyncStatus){
-									// Get pasted string
-									auto	string = asyncOperation.GetResults();
-
-									// Switch to UI thread
-									textBox.DispatcherQueue().TryEnqueue([textBox, string, isAllowedProc](){
-										// Check if is allowed
-										if (!isAllowedProc(string))
-											// Not allowed
-											return;
-
-										// Setup
-										std::basic_string<TCHAR>	currentText = textBox.Text().c_str();
-										auto						selectionStart = textBox.SelectionStart();
-										auto						selectionLength = textBox.SelectionLength();
-
-										// Update
-										sTextBoxInUpdate = textBox;
-										textBox.Text(
-											(currentText.substr(0, selectionStart) +
-															(std::basic_string<TCHAR>) string +
-															currentText.substr(selectionStart + selectionLength))
-													.substr(0, textBox.MaxLength()));
-										textBox.SelectionStart(selectionStart + (int32_t) string.size());
-										textBox.SelectionLength(0);
-										sTextBoxInUpdate = nullptr;
-									});
-								});
-
-				// Handled
-				textControlPasteEventArgs.Handled(true);
-			});
 	getTextBox().TextChanging(
-			[filterProc]
+			[filterProc, wasFilteredProc]
 			(const IInspectable& sender, const TextBoxTextChangingEventArgs& textBoxTextChangingEventArgs){
 				// Check if handling events
 				if (sender == sTextBoxInUpdate)
@@ -161,6 +108,14 @@ TextBoxHelper& TextBoxHelper::setTextValidationProcs(std::function<bool(const hs
 					sTextBoxInUpdate = textBox;
 					textBox.Text(filteredString);
 					sTextBoxInUpdate = nullptr;
+
+					// Caret to end (setting Text resets it to the start; macOS leaves it at the end)
+					textBox.SelectionStart((int32_t) filteredString.size());
+					textBox.SelectionLength(0);
+
+					// Note was filtered
+					if (wasFilteredProc)
+						wasFilteredProc();
 				}
 		});
 
